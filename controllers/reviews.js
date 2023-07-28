4d00af3a74856371083b804413f129c9d97ef729
Terelld <terelld@gmail.com>
2023-07-28 11:47:55 -0500

    create edit button that leads to form to edit and update review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,7 +6,9 @@ const {OpenMic, Review} = require("../models/open-mic");
 
 module.exports = {
     create,
-    deleteReview, 
+    deleteReview,
+    editReview, 
+    updateReview,
 };
 
 
@@ -43,7 +45,39 @@ async function create(req, res) {
     console.log('review removed', deleteReview);
 }
 
+async function editReview(req, res) {
+  try {
+    const openMic = await OpenMic.findOne({ _id: req.params.id });
 
-  
+    if (!openMic) {
+      return res.redirect('/open-mics');
+    }
+    const reviewToEdit = openMic.reviews.id(req.params.review_id);
+
+    if (!reviewToEdit) {
+      return res.redirect(`/open-mics/${openMic._id}`);
+    }
+
+    res.render('open-mics/edit', { openMic, review: reviewToEdit });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
+
+
+async function updateReview(req, res) {
+  const openMic = await OpenMic.findById(req.params.id);
+  const reviewToUpdate = openMic.reviews.id(req.params.review_id);
+
+  try {
+    reviewToUpdate.updateOne();
+    await openMic.save(); 
+  }catch (err) {
+    console.log(err);
+  }
+  res.redirect(`/open-mics/${openMic._id}`);
+}
+
 
   
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,4 +6,9 @@ router.post('/:id/reviews', reviewsCtrl.create);
 
 router.post('/:id/reviews/:review_id/delete', reviewsCtrl.deleteReview);
 
+router.get('/:id/reviews/:review_id/edit', reviewsCtrl.editReview);
+
+// Route for updating a specific review
+router.post('/:id/reviews/:review_id/update', reviewsCtrl.updateReview);
+
 module.exports = router;
